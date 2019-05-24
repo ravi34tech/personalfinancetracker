c44c743b5131f20d346056e9cbb6a654023eb238
rmummadu <rmummadu@cisco.com>
2019-05-24 15:25:32 +0530

    First commit

---
+++ src/main/java/com/ravi/fintrack/PersonalfinancetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.fintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalfinancetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalfinancetrackerApplication.class, args);
+	}
+
+}
